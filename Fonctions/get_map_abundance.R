de361fbda5dda310f137f3e7e2006597fafd1b65
Léa Pautrel <pautrel.lea@gmail.com>
2021-01-11 11:49:50 +0100

    Création de la fonction pour obtenir la carte d'abondance

---
+++ Fonctions/get_map_abundance.R
@@ -0,0 +1,40 @@
+get_map_abundance <- function(empty.map,
+															dsm.pred,
+															predata_tmp,
+															session_selec,
+															segdata,
+															distdata){
+	# Carte vide
+	res <- empty.map +
+		
+		# Raster d'abondance des requins
+		geom_raster(data = as.data.frame(
+			cbind(
+				x = predata_tmp$longitude,
+				y = predata_tmp$latitude,
+				abondance = dsm.pred
+			)
+		),
+		aes(x = x, y = y, fill = abondance)) +
+		
+		# Lignes des transects
+		geom_path(
+			data = segdata,
+			mapping = aes(x = longitude, y = latitude, group = Transect.Label),
+			size = 0.3,
+			colour = alpha("black", 0.2)
+		) +
+		
+		# Points de la session
+		geom_point(data = (distdata %>% filter(session == session_selec)),
+							 aes(x = longitude, y = latitude, col = session)) +
+		
+		# Thème et couleurs
+		theme_void() +
+		scale_fill_viridis() +
+		scale_color_manual(values = wesanderson::wes_palette("BottleRocket1", n = 4)) +
+		coord_fixed(ratio = 1)
+	
+	return(res)
+	
+}
