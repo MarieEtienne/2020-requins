66534daad8a13778f7a0f45090112493ebf627cc
Léa Pautrel <pautrel.lea@gmail.com>
2021-01-13 17:30:55 +0100

    update poster + cartes + plot fonction détection

--- Fonctions/get_map_abundance.R
+++ Fonctions/get_map_abundance.R
@@ -17,29 +17,28 @@ get_map_abundance <- function(empty.map,
 			)
 		),
 		aes(x = x, y = y, fill = abondance)) +
-
-		# Lignes des transects
-		geom_path(
-			data = segdata,
-			mapping = aes(x = longitude, y = latitude, group = Transect.Label),
-			size = 0.3,
-			colour = alpha("black", 0.2)
-		) +
-
-		# Points de la session
-		geom_point(data = (distdata %>% filter(session == session_selec)),
-							 aes(x = longitude, y = latitude, col = session)) +
+# 
+# 		# Lignes des transects
+# 		geom_path(
+# 			data = segdata,
+# 			mapping = aes(x = longitude, y = latitude, group = Transect.Label),
+# 			size = 0.3,
+# 			colour = alpha("black", 0.2)
+# 		) +
+# 
+# 		# Points de la session
+# 		geom_point(data = (distdata %>% filter(session == session_selec)),
+# 							 aes(x = longitude, y = latitude, col = session)) +
 
 		# Thème et couleurs
 		theme_void() +
-		theme(legend.position = "bottom",
-					legend.background = element_rect(fill = "#D7E0E0", color = NA)) +
+		theme(legend.position = "bottom") +
 		scale_fill_gradientn(colours = pal, name = " ") +
 		scale_color_manual(values = wesanderson::wes_palette("BottleRocket1", n = 4)) +
 		coord_fixed(ratio = 1.5) +
 		guides(col = FALSE,
-					 fill = guide_colourbar(ticks = F, barwidth = 10, barheight = 1,
-					 											 label.theme = element_text(size = 12),
+					 fill = guide_colourbar(ticks = F, barwidth = 30, barheight = 4,
+					 											 label.theme = element_text(color = "#4B5755", size = 50),
 					 											 direction = "horizontal"))
 	
 	return(res)
