a5c73c43edd95f4d67667c538d6c76d2a3b94c99
Léa Pautrel <pautrel.lea@gmail.com>
2021-01-13 10:59:55 +0100

    update maps

--- Fonctions/get_map_abundance.R
+++ Fonctions/get_map_abundance.R
@@ -3,10 +3,11 @@ get_map_abundance <- function(empty.map,
 															predata_tmp,
 															session_selec,
 															segdata,
-															distdata){
+															distdata,
+															pal){
 	# Carte vide
 	res <- empty.map +
-		
+
 		# Raster d'abondance des requins
 		geom_raster(data = as.data.frame(
 			cbind(
@@ -16,7 +17,7 @@ get_map_abundance <- function(empty.map,
 			)
 		),
 		aes(x = x, y = y, fill = abondance)) +
-		
+
 		# Lignes des transects
 		geom_path(
 			data = segdata,
@@ -24,16 +25,19 @@ get_map_abundance <- function(empty.map,
 			size = 0.3,
 			colour = alpha("black", 0.2)
 		) +
-		
+
 		# Points de la session
 		geom_point(data = (distdata %>% filter(session == session_selec)),
 							 aes(x = longitude, y = latitude, col = session)) +
-		
+
 		# Thème et couleurs
 		theme_void() +
-		scale_fill_viridis() +
+		# scale_fill_viridis() +
+		scale_fill_gradientn(colours = pal, name = " ") +
 		scale_color_manual(values = wesanderson::wes_palette("BottleRocket1", n = 4)) +
-		coord_fixed(ratio = 1)
+		coord_fixed(ratio = 1.5) +
+		guides(col = FALSE,
+					 fill = guide_colourbar(barwidth = 0.5, barheight = 10))
 	
 	return(res)
 	
