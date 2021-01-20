33a3767319962e0d6f153476e6c9da83cd0047c7
Rindra Ranaivomanana <rindra.fanomezana@gmail.com>
2021-01-20 15:31:33 +0100

    nouvelle partie spring summer

--- shiny-app/ui.R
+++ shiny-app/ui.R
@@ -53,35 +53,8 @@ ui = shiny::htmlTemplate(
   # NOS CARTES##############################################################
   session2 = plotlyOutput("session2"),
 
-  session3 = plotlyOutput("session3"),
+  session3 = plotlyOutput("session3")
   
-  ###########################################################################
-  
-  # City Selector
-  city_selector = selectInput(
-    "city", 
-    label = "Select City", 
-    choices = d_clean$city %>% 
-      unique(),
-    selected = "Auckland"
-    ),
-  
-  
-  # Selector for Time
-  time_selector = sliderInput(
-    "time", 
-    "Date",
-    min(d_routes$request_time) %>% as.Date(), 
-    max(d_routes$request_time) %>% as.Date(),
-    value = max(d_routes$request_time) %>% as.Date(),
-    step = 30,
-    animate = animationOptions(
-      playButton = HTML("<img src='images/icons/play-button.png' height='42' width='42'>"), 
-      pauseButton = HTML("<img src='images/icons/pause-button.png' height='42' width='42'>")
-      )
-  ),
-  
-  # Leaflet map
-  leaflet_map = leafletOutput(outputId = "map") %>% 
-    withSpinner(color="#0dc5c1")
   )
+  
+  
